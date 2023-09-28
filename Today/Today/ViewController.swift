5d9430d74866eea37938dd21207f23675714c1f1
JH713 <strider13@snu.ac.kr>
2023-09-28 19:18:36 +0900

    Init: Today 프로젝트 생성

---
+++ Today/Today/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 이지현 on 2023/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
